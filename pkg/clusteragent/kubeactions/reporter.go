9b5a6ce43964aab901eedbc92598137da20cda5d
Alexandre Lavigne <alexandre.lavigne@datadoghq.com>
2026-05-11 10:59:32 +0200

    fixup! [kubeactions] increase payload size to fit larger resources

--- pkg/clusteragent/kubeactions/reporter.go
+++ pkg/clusteragent/kubeactions/reporter.go
@@ -9,7 +9,6 @@ package kubeactions
 
 import (
 	"encoding/json"
-	"maps"
 	"time"
 
 	kubeactions "github.com/DataDog/agent-payload/v5/kubeactions"
@@ -105,14 +104,14 @@ func (r *ResultReporter) report(actionKey ActionKey, action *kubeactions.KubeAct
 		RequestedBy:       action.GetRequestedBy(),
 		Timestamp:         ts.Format(time.RFC3339),
 		Message:           msg,
-		Payloads:          maps.Clone(payloads),
+		Payloads:          payloads,
 		ClusterName:       r.clusterName,
 		ResourceKind:      resourceKind,
 		ResourceName:      resourceName,
 		ResourceNamespace: resourceNamespace,
 	}
 
-	log.Infof("[KubeActions] Sending EVP event: event_type=%s, action_id=%s, status=%s nr_payloads=", evpEventType, event.ActionID, event.Status, len(event.Payloads))
+	log.Infof("[KubeActions] Sending EVP event: event_type=%s, action_id=%s, status=%s nr_payloads=%d", evpEventType, event.ActionID, event.Status, len(event.Payloads))
 
 	payload, err := json.Marshal(event)
 	if err != nil {
