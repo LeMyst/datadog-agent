88a5c35fdc0fa936ffdf33d3fcac418c5ea4fbf3
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-05-15 18:50:16 +0000

    refactor(collector): migrate comp/collector/collector to V2 component architecture
    - Add comp/collector/collector/def/ with Component interface, EventType, EventReceiver, and NoneModule
    - Add comp/collector/collector/impl/ with implementation moved from collectorimpl/
    - Add comp/collector/collector/fx/ and comp/collector/collector/mock/ shims
    - Update root comp/collector/collector package to be a deprecated shim using type aliases
    - Update all callers to import from def/ and impl/ sub-packages
    - Add BUILD.bazel files for all new packages

--- comp/core/flare/flare_test.go
+++ comp/core/flare/flare_test.go
@@ -21,7 +21,7 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/demultiplexerimpl"
-	"github.com/DataDog/datadog-agent/comp/collector/collector"
+	collector "github.com/DataDog/datadog-agent/comp/collector/collector/def"
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery"
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/autodiscoveryimpl"
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/scheduler"
